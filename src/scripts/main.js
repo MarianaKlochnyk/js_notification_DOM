895f25a2a70e80c61d886e2f54c5a23dc7d2f76f
Mariana Klochnyk <klochnykmaryana@gmail.com>
2026-03-16 13:33:20 +0200

    edit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,32 @@
 'use strict';
 
 const pushNotification = (posTop, posRight, title, description, type) => {
-  // write code here
+  const notification = document.createElement('div');
+
+  notification.classList.add('notification', type);
+
+  const messageTitle = document.createElement('h2');
+
+  messageTitle.classList.add('title');
+  messageTitle.textContent = title;
+
+  const messageParagraph = document.createElement('p');
+
+  messageParagraph.classList.add('description');
+  messageParagraph.textContent = description;
+
+  notification.appendChild(messageTitle);
+  notification.appendChild(messageParagraph);
+
+  notification.style.position = 'absolute';
+  notification.style.left = posRight + 'px';
+  notification.style.top = posTop + 'px';
+
+  document.body.appendChild(notification);
+
+  setTimeout(() => {
+    notification.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
